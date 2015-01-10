06cb3f7dccb708214fc3ba034aea28474689dc12
IamTechknow <techhexium@gmail.com>
2015-01-09 23:22:03 -0800

    Working prototype app centered over Watsonville

--- src/com/ucschackathon/app/TrailActivity.java
+++ src/com/ucschackathon/app/TrailActivity.java
@@ -2,14 +2,40 @@ package com.ucschackathon.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class TrailActivity extends Activity {
-	/**
-	 * Called when the activity is first created.
-	 */
+
+	static final LatLng WATSONVILLE = new LatLng(36.9154033, -121.7694327);
+	private GoogleMap map;
+
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+				.getMap();
+		Marker hamburg = map.addMarker(new MarkerOptions().position(WATSONVILLE)
+				.title("Watsonville"));
+
+		// Move the camera instantly to hamburg with a zoom of 15.
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(WATSONVILLE, 15));
+
+		// Zoom in, animating the camera.
+		map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
