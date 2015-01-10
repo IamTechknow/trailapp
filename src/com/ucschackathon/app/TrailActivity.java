ac8ef2fc7848b235f6124319bb47f8ea915ee99d
IamTechknow <techhexium@gmail.com>
2015-01-10 12:44:48 -0800

    Start code to obtain KML file to use onto the map

--- src/com/ucschackathon/app/TrailActivity.java
+++ src/com/ucschackathon/app/TrailActivity.java
@@ -1,9 +1,19 @@
 package com.ucschackathon.app;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -14,7 +24,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class TrailActivity extends Activity {
 
 	private static final LatLng WATSONVILLE = new LatLng(36.9154033, -121.7694327);
+	private static final String theKML = "http://www.watsonvillewetlandswatch.org/sloughs/EntireMapWeb.kml";
 	private GoogleMap map;
+	private InputStream is;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +34,42 @@ public class TrailActivity extends Activity {
 		setContentView(R.layout.main);
 		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
 				.getMap();
-		Marker hamburg = map.addMarker(new MarkerOptions().position(WATSONVILLE)
+		Marker watsonville = map.addMarker(new MarkerOptions().position(WATSONVILLE)
 				.title("Watsonville"));
 
-		// Move the camera instantly to Watsonville with a zoom of 20.
-		map.moveCamera(CameraUpdateFactory.newLatLngZoom(WATSONVILLE, 20));
+		// Move the camera instantly to hamburg with a zoom of 15.
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(WATSONVILLE, 15));
 
 		// Zoom in, animating the camera.
 		map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
+		//Get access to the KML file by setting up an URL connection
+
+		ConnectivityManager connMgr = (ConnectivityManager)
+				getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		if (networkInfo != null && networkInfo.isConnected()) {
+			try {
+				URL url = new URL(theKML);
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setReadTimeout(10000 /* milliseconds */);
+				conn.setConnectTimeout(15000 /* milliseconds */);
+				conn.setRequestMethod("GET");
+				conn.setDoInput(true);
+				// Starts the query
+				conn.connect();
+				int response = conn.getResponseCode();
+				Log.d("TrailActivity", "The response is: " + response);
+				is = conn.getInputStream();
+				//Code to use the kml file here
+
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		else //Let the user know there's no connection
+			Toast.makeText(this,R.string.noconnectionID, Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
