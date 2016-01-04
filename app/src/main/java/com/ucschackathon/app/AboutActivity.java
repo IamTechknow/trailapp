5bf533480f7e1915d66eeca419aff29fc80d50b3
IamTechknow <techhexium@gmail.com>
2016-01-04 11:14:14 -0800

    Implemented ancestral nav

--- app/src/main/java/com/ucschackathon/app/AboutActivity.java
+++ app/src/main/java/com/ucschackathon/app/AboutActivity.java
@@ -1,8 +1,11 @@
 package com.ucschackathon.app;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.text.method.LinkMovementMethod;
 import android.text.Html;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -20,9 +23,25 @@ public class AboutActivity extends AppCompatActivity {
         Toolbar mToolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(mToolbar);
 
+        ActionBar bar = getSupportActionBar();
+        if(bar != null)
+            bar.setDisplayHomeAsUpEnabled(true);
+
         String mAbout = getString(R.string.aboutText);
         TextView mTv = (TextView)findViewById(R.id.about);
         mTv.setText(Html.fromHtml(mAbout));
         mTv.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (NavUtils.getParentActivityName(this) != null)
+                    NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/ucschackathon/app/ListMarkersActivity.java
+++ app/src/main/java/com/ucschackathon/app/ListMarkersActivity.java
@@ -1,11 +1,14 @@
 package com.ucschackathon.app;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 
 import com.ucschackathon.app.adapter.MarkerAdapter;
 
@@ -27,11 +30,27 @@ public class ListMarkersActivity extends AppCompatActivity {
         Toolbar mToolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(mToolbar);
 
+        ActionBar bar = getSupportActionBar();
+        if(bar != null)
+            bar.setDisplayHomeAsUpEnabled(true);
+
         mHelper = new TrailDatabaseHelper(getApplicationContext());
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new MarkerAdapter(mHelper.queryMarkers(), getResources()));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (NavUtils.getParentActivityName(this) != null)
+                    NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
